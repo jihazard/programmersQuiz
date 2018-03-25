1b3708832cb73d8db6dc9f1d8db173ce53b2408f
jihwan <yoonjh238@gmail.com>
2018-03-25 16:05:27 +0900

    홀수와 짝수  레벨1 자바
    evenOrOdd 메소드는 int형 num을 매개변수로 받습니다.
    num이 짝수일 경우 Even을 반환하고 홀수인 경우 Odd를 반환하도록 evenOrOdd에 코드를 작성해 보세요.
    num은 0이상의 정수이며, num이 음수인 경우는 없습니다.

---
+++ src/EvenOdd/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2017 (C) by jihwan  All right reserved.
+ *
+ * @FileName  package-info.java
+ * @Discription : 
+ *
+ * @Modification Information
+ * @
+ * @  수정일                  수정자                     수정내용
+ * @ ---------   ---------   -------------------------------
+ * @ 2018. 3. 25.      jihwan      최초생성
+ *
+ *
+ * @author jihwan
+ * @since  2018. 3. 25.
+ * @version 1.0
+ * @see
+ */
+/**
+ * @Project  : JINHAP 공정품질 통합 모니터링 시스템
+ * @author   : jihwan
+ *
+ */
+package EvenOdd;
